8f2a186eb8ca4c5c6ba4f87003b9f147ef87dfac
AishaOthman <75293286+AishaOthman@users.noreply.github.com>
2022-06-23 09:46:31 +0300

    Add files via upload

---
+++ Component/NanvSearhbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NanvSearhbar =()=>{
+    return(
+        <div className=header>
+            <h1>Welcome To TripHunt</h1>
+            <Logo/>
+
+        </div>
+    )
+}
+export default Header;
